9318fa975d16b5991820c0ea3690906de35208b8
Derlys Dominguez <derlys275@gmail.com>
2020-07-23 13:39:06 -0500

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UiBrand } from '@kikstart-ui/ui-brand';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'box';
+  title = 'site';
+  brand: UiBrand = {
+    name: 'Site',
+    logo: 'https://github.com/kikstart-ui.png',
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutWebModule } from '@kikstart-ui/layout-web';
+import { UiNavbarModule } from '@kikstart-ui/ui-navbar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutWebModule, UiNavbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
